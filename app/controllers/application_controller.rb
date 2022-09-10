99725229f6d45de2c2658e1dd9e9fc83616d335b
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-09-10 12:16:38 +0300

    getting crimes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+   get "/crimes" do
+    crimes=Crime.all 
+    crimes.to_json(include: :police_station)
+  end
 
 end
